11e08b075615e082a0ab8497ae10bbac66bb03ba
Matthew Fricker <matthewfricker@macbook-air.myfiosgateway.com>
2021-10-09 18:06:15 -0400

    Update the docker env variable names

--- memory-marker-api/database.js
+++ memory-marker-api/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
-                                process.env.POSTGRES_USER || 'postgres',
-                                process.env.POSTGRES_PASSWORD || 'postgres',
+                                process.env.DB_USER || 'postgres',
+                                process.env.DB_PASSWORD || 'postgres',
                                 {
                                     host: process.env.DB_HOST || 'localhost',
                                     port: process.env.DB_PORT || 5432,
